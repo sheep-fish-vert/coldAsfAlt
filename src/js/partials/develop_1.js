8d5b3d748698f44df458ded8a96aeb01f5329293
Dmitry <dmitry1939@gmail.com>
2016-02-23 16:03:01 +0200

    instructions done

--- src/js/partials/develop_1.js
+++ src/js/partials/develop_1.js
@@ -1,6 +1,45 @@
 try{
 
+    function instructionSlider(){
+
+        function instructionSliderInit(windowWidth){
+
+            var slickTag = $('.list-main');
+
+            if(windowWidth<=992){
+                if(!slickTag.is('.slick-slider')){
+                    slickTag.slick({
+                        slidesToShow: 1,
+                        dots:true,
+                        arrows:false,
+                        draggable:false
+                    });
+                }
+            }
+            else{
+                if(slickTag.is('.slick-slider')){
+                    slickTag.slick('unslick');
+                }
+            }
+        }
+
+        var windowWidth = $(window).width();
+
+        instructionSliderInit(windowWidth);
+
+        $(window).resize(function(){
+
+            windowWidth = $(window).width();
+
+            instructionSliderInit(windowWidth);
+
+        });
+
+    };
+
     $(document).ready(function(){
+
+        instructionSlider();
 
     });
 
